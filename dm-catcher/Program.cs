8ff09a721edcd6a61f43b6f85b30b5eb68586e7f
Noahkiq <noahkiqgaming@gmail.com>
2017-01-16 12:01:36 -0500

    Did the actual code

--- dm-catcher/Program.cs
+++ dm-catcher/Program.cs
@@ -1,44 +1,52 @@
 ﻿using System;
 using Discord;
 using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Microsoft.CSharp;
+using Newtonsoft.Json.Linq;
 
 namespace dmcatcher
 {
 	class dmcatcher
 	{
-		static void Main(string[] args) => new Program().Start();
-
-		public void LoadJson()
-		{
-			using (StreamReader r = new StreamReader("config.json"))
-			{
-				string json = r.ReadToEnd();
-				List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);
-			}
-		}
-
-		public class Item
-		{
-			public string token;
-			public List<KeyValuePair> alertChannels;
-			public ulong ownerId;
-		}
+		static void Main(string[] args) => new dmcatcher().Start();
 
 		private DiscordClient _client;
 
 		public void Start()
 		{
+			string currentDir = Directory.GetCurrentDirectory();
+			string configJson = File.ReadAllText(currentDir + "/config.json");
+			dynamic data = JObject.Parse(configJson);
+			string token = data.token;
+			ulong alertGuild = data.alertGuild;
+			ulong alertChannel = data.alertChannel;
+
 			_client = new DiscordClient();
 
 			_client.Log.Message += (s, e) => Console.WriteLine($"[{e.Severity}] {e.Source}: {e.Message}");
 
 			_client.MessageReceived += async (s, e) =>
 			{
-				// hi
+				if (e.Channel.IsPrivate)
+				{
+					var mutualServers = new List<KeyValuePair<string, ulong>>();
+					var servers = _client.Servers;
+
+					foreach(Server server in servers)
+						foreach (User user in server.Users)
+							if (user.Id == e.User.Id)
+								mutualServers.Add(new KeyValuePair<string, ulong>(server.Name, server.Id));
+
+					string mutuals = string.Join(", ", mutualServers.ToArray());
+					await _client.GetServer(alertGuild).GetChannel(alertChannel).SendMessage($"PM from (Id {e.User.Id})\n({e.User.Mention}) {e.User.Name} [#{e.User.Discriminator}]\nMutual servers: {mutuals}\n**Message:**\n" + e.Message.Text);
+				}
 			};
 				
 			_client.ExecuteAndWait(async () => {
-				await _client.Connect(Item.token, TokenType.User);
+				await _client.Connect(token, TokenType.User);
+				Console.WriteLine($"Connected as {_client.CurrentUser.Name}#{_client.CurrentUser.Discriminator}");
 			});
 
 		}
